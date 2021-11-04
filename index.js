2123a23d5431d2748a30f9a65297c390586d6c0f
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2021-11-04 13:03:47 +0100

    Avoid Chrome headless to be detected

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
 const userAgent = require("user-agents");
+
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+puppeteer.use(StealthPlugin());
 
 (async () => {
   const browser = await puppeteer.launch();
